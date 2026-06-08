4aed9cb76a40d95ae23c982e63e7f5421894d065
robaya <robaya05@gmail.com>
2026-06-08 21:24:27 +0700

    alhikam 08/06/2026

--- resources/views/admin/candidates/show.blade.php
+++ resources/views/admin/candidates/show.blade.php
@@ -798,7 +798,7 @@
 
             let text = "";
             text += center + boldOn + "BUKTI PEMBAYARAN\n" + boldOff;
-            text += "PSB PONPES ASSA'ADAH\n";
+            text += "PSB PONPES AL-HIKAM\n";
             text += "--------------------------------\n";
             text += left;
             text += "No Invoice : #" + data.invoice + "\n";
